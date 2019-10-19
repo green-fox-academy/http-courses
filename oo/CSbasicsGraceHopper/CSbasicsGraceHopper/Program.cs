94c967491c6d62967fb10c48eba43811d3000cc1
Kond <peter.tamas@greenfoxacademy.com>
2019-10-19 10:46:34 +0200

    basic C# program 1. megoldas

---
+++ oo/CSbasicsGraceHopper/CSbasicsGraceHopper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSbasicsGraceHopper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int max = 0;
+
+            do
+            {
+                Console.Write("Szam: ");
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+
+                if (number > max)
+                {
+                    max = number;
+                }
+
+                Console.WriteLine();
+
+            } while (sum <= 20);
+
+            Console.WriteLine($"A legnagyobb szam: {max}");
+        }
+    }
+}
