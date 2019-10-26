83f79031a38b7f908a5f2a3c5f2fea31d9398bb8
Kond <peter.tamas@greenfoxacademy.com>
2019-10-26 11:25:18 +0200

    uj DOM elemek hozzadasa

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 let paragraphs = document.getElementsByClassName('bekezdes');
-let paragraphs2 = document.querySelectorAll('.bekezdes');
+let paragraphs2 = document.querySelectorAll('.bekezdes, .masik');
 
 for (let p of paragraphs2) {
   p.innerHTML = parseInt(p.innerHTML) + 1;
 }
 
+paragraphs2[paragraphs2.length - 1].innerHTML += '<a href="#">link</a>';
+
+const link = document.createElement('a');
+link.innerHTML = 'link2';
+link.href = 'http://index.hu';
+paragraphs2[paragraphs2.length - 1].appendChild(link);
+
 const cim = document.getElementById('cim').innerHTML;
+
 console.log(cim);
