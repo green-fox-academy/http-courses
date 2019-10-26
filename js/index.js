dfebeb65b8ffa6f89309e05195c681ab0588d65b
Kond <peter.tamas@greenfoxacademy.com>
2019-10-26 11:15:50 +0200

    getElementById pelda

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 let paragraphs = document.getElementsByClassName('bekezdes');
 for (let p of paragraphs) {
-  // console.log(p.innerHTML);
-  console.log(p.textContent);
-  
   p.innerHTML = parseInt(p.innerHTML) + 1;
 }
-console.log(paragraphs);
+
+const cim = document.getElementById('cim').innerHTML;
+console.log(cim);
