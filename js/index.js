fa5f7527079141c59653451bc323545ba73c42ed
Kond <peter.tamas@greenfoxacademy.com>
2019-10-26 11:54:32 +0200

    window.onload hasznalat

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
-let paragraphs = document.getElementsByClassName('bekezdes');
-let paragraphs2 = document.querySelectorAll('.bekezdes, .masik');
+window.onload = domManipulation();
 
-for (let p of paragraphs2) {
-  p.innerHTML = parseInt(p.innerHTML) + 1;
-  p.onclick = () => alert(p.innerHTML);
+function domManipulation() {
+  let paragraphs = document.getElementsByClassName('bekezdes');
+  let paragraphs2 = document.querySelectorAll('.bekezdes, .masik');
+
+  for (let p of paragraphs2) {
+    p.innerHTML = parseInt(p.innerHTML) + 1;
+    p.onclick = () => alert(p.innerHTML);
+  }
+
+  paragraphs2[paragraphs2.length - 1].innerHTML += '<a href="#">link</a>';
+
+  const link = document.createElement('a');
+  link.innerHTML = 'link2';
+  link.href = 'http://index.hu';
+  paragraphs2[paragraphs2.length - 1].appendChild(link);
+
+  const cim = document.getElementById('cim').innerHTML;
 }
-
-paragraphs2[paragraphs2.length - 1].innerHTML += '<a href="#">link</a>';
-
-const link = document.createElement('a');
-link.innerHTML = 'link2';
-link.href = 'http://index.hu';
-paragraphs2[paragraphs2.length - 1].appendChild(link);
-
-const cim = document.getElementById('cim').innerHTML;
 
 function printHello() {
   console.log('hello');
 }
-
-
