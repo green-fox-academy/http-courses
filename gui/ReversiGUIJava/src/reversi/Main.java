1314c589c03d3e7250d7ea7dd0ef7958b420c107
Kond <peter.tamas@greenfoxacademy.com>
2019-11-30 15:59:02 +0100

    tabla megjelenitese

--- gui/ReversiGUIJava/src/reversi/Main.java
+++ gui/ReversiGUIJava/src/reversi/Main.java
@@ -2,17 +2,23 @@ package reversi;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static final int width = 400;
+    private static final int height = 400;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Tabla tabla = new Tabla("assets/allas.txt");
+        Group root = new Group();
+        primaryStage.setTitle("Reversi");
+        primaryStage.setScene(new Scene(root, width, height));
+        tabla.megjelenit(root, width, height);
         primaryStage.show();
     }
 
--- gui/ReversiGUIJava/src/reversi/Tabla.java
+++ gui/ReversiGUIJava/src/reversi/Tabla.java
@@ -1,5 +1,11 @@
 package reversi;
 
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,12 +14,43 @@ import java.util.List;
 
 public class Tabla {
 
-  char[][] allas;
+  private char[][] allas;
 
   public Tabla(String fajlNev) {
     List<String> fajlTartalom = fajlBeolvasas(fajlNev);
     allas = allasInicializalas(fajlTartalom);
 
+  }
+
+  public void megjelenit(Group group, int szelesseg, int magassag) {
+    Paint hatterSzin = Paint.valueOf(Color.LIGHTGRAY.toString());
+    Rectangle hatter = new Rectangle(szelesseg, magassag, hatterSzin);
+    group.getChildren().add(hatter);
+
+    int mezoMeret = Math.min(szelesseg, magassag) / allas.length;
+    int terkoz = 5;
+    int korongRadius = (mezoMeret - 2 * terkoz) / 2;
+
+    for (int sor = 0; sor < allas.length; sor++) {
+      for (int oszlop = 0; oszlop < allas[sor].length; oszlop++) {
+        int x = oszlop * mezoMeret + terkoz + korongRadius;
+        int y = sor * mezoMeret + terkoz + korongRadius;
+        Color szin = korongSzin(allas[sor][oszlop]);
+        Paint korongSzin = Paint.valueOf(szin.toString());
+        Circle korong = new Circle(x, y, korongRadius, korongSzin);
+        group.getChildren().add(korong);
+      }
+    }
+  }
+
+  private Color korongSzin(char c) {
+    if (c == 'F') {
+      return Color.WHITE;
+    } else if (c == 'K') {
+      return Color.BLUE;
+    } else {
+      return Color.DARKGRAY;
+    }
   }
 
   private char[][] allasInicializalas(List<String> fajlTartalom) {
