49694bd43e15b6e7c375fbe88994cd76a00e78a3
Kond <peter.tamas@greenfoxacademy.com>
2019-12-19 19:00:03 +0100

    8. feladat

--- megoldas-kozep-szint/EgyszamjatekGUI/src/egyszamjatek/Controller.java
+++ megoldas-kozep-szint/EgyszamjatekGUI/src/egyszamjatek/Controller.java
@@ -1,4 +1,23 @@
 package egyszamjatek;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+
+  @FXML private TextField tipsTextField;
+  @FXML private Label tipsLabel;
+
+  @FXML
+  public void updateTipsCount() {
+    String tipsText = tipsTextField.getText();
+    if (tipsText.isEmpty()) {
+      tipsLabel.setText("0 db");
+      return;
+    }
+
+    int tipsCount = tipsText.split(" ").length;
+    tipsLabel.setText(tipsCount + " db");
+  }
+
 }
