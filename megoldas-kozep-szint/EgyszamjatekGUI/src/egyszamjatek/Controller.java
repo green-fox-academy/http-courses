646ce389e140ba76ddc8bfe7f8f073e1e11b2215
Kond <peter.tamas@greenfoxacademy.com>
2019-12-19 18:51:11 +0100

    7. feladat

---
+++ megoldas-kozep-szint/EgyszamjatekGUI/src/egyszamjatek/Controller.java
@@ -0,0 +1,4 @@
+package egyszamjatek;
+
+public class Controller {
+}
---
+++ megoldas-kozep-szint/EgyszamjatekGUI/src/egyszamjatek/Main.java
@@ -0,0 +1,23 @@
+package egyszamjatek;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Egyszamjatek GUI");
+        primaryStage.setScene(new Scene(root, 496, 187));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
