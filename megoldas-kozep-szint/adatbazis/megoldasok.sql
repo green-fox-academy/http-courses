70454b70c1288a3a2a9464be3d376f8b0cab62fd
Kond <peter.tamas@greenfoxacademy.com>
2019-12-20 16:55:12 +0100

    [adatbazis] kezdeti allapot

---
+++ megoldas-kozep-szint/adatbazis/megoldasok.sql
@@ -0,0 +1,17 @@
+-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+-- 1. feladat:
+
+
+-- 3. feladat:
+
+
+-- 4. feladat:
+
+
+-- 5. feladat:
+
+
+-- 6. feladat:
+
