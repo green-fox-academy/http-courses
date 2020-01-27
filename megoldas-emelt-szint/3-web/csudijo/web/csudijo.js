10625334dcb3b52cfdc19cd0113e61b275f649ea
Kond <peter.tamas@greenfoxacademy.com>
2020-01-27 17:04:44 +0100

    13. feladat

--- megoldas-emelt-szint/3-web/csudijo/web/csudijo.js
+++ megoldas-emelt-szint/3-web/csudijo/web/csudijo.js
@@ -5,4 +5,21 @@ async function legnepszerubbEtel() {
   document.getElementById('legnepszerubb').innerHTML = etelNev;
 }
 
+async function vendegkonyv() {
+  const bejegyzes = document.getElementById('bejegyzes').value;
+  const valasz = await fetch('api/vendegkonyv', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({ bejegyzes }),
+  });
+
+  if (valasz.status == 200) {
+    document.getElementById('bejegyzes').value = '';
+    alert('Köszönjük a bejegyzését!');
+  }
+}
+
 window.onload = legnepszerubbEtel();
