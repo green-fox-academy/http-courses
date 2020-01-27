a98b1eba148c1a0e283a69cfad6f1e11ebe5767a
Kond <peter.tamas@greenfoxacademy.com>
2020-01-27 18:09:39 +0100

    16. feladat

--- megoldas-emelt-szint/3-web/csudijo/lekerdezesek/lekerdezesek.sql
+++ megoldas-emelt-szint/3-web/csudijo/lekerdezesek/lekerdezesek.sql
@@ -5,6 +5,9 @@ CREATE DATABASE `csudijo` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
 
 ***
 16. feladat
+SELECT COUNT(etel) AS "etelek-szama"
+FROM termekek
+WHERE etel = 1;
 
 ***
 17. feladat
