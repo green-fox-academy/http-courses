7b3bb3517b5c106796bf4d99012f4dce6e8d05f9
Kond <peter.tamas@greenfoxacademy.com>
2020-01-28 18:01:12 +0100

    Reversi Java 3. feladat

--- megoldas-emelt-szint/2-java/ReversiGui/src/reversi/Main.java
+++ megoldas-emelt-szint/2-java/ReversiGui/src/reversi/Main.java
@@ -6,10 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Arrays;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Tabla tabla = new Tabla("allas.txt");
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
--- megoldas-emelt-szint/2-java/ReversiGui/src/reversi/Tabla.java
+++ megoldas-emelt-szint/2-java/ReversiGui/src/reversi/Tabla.java
@@ -1,7 +1,37 @@
 package reversi;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabla {
 
-  char[][] allas = new char[8][8];
+  char[][] allas;
+
+  public Tabla(String fajlNev) {
+    List<String> fajlTartalom = fajlBeolvasas(fajlNev);
+    allas = kezdoAllas(fajlTartalom);
+  }
+
+  private char[][] kezdoAllas(List<String> fajlTartalom) {
+    if (fajlTartalom.isEmpty()) return new char[8][8];
+
+    char[][] allas = new char[fajlTartalom.size()][fajlTartalom.get(0).length()];
+    for (int i = 0; i < fajlTartalom.size(); i++) {
+      allas[i] = fajlTartalom.get(i).toCharArray();
+    }
+    return allas;
+  }
+
+  private List<String> fajlBeolvasas(String fajlNev) {
+    try {
+      return Files.readAllLines(Paths.get(fajlNev));
+    } catch (IOException e) {
+      System.err.println("Nem sikerult beolvasni a fajlt :(");
+      return new ArrayList<>();
+    }
+  }
 
 }
