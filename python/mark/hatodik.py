7cb2b552808a4d1af6e2b499c856c9e4c4c1a493
markkovari <kovarimarkofficial@gmail.com>
2020-04-25 13:30:53 +0200

    megoldasok hatodik es hetedikhez

--- python/mark/hatodik.py
+++ python/mark/hatodik.py
@@ -1,3 +1,5 @@
+# egy jo megoldas
+
 # matrixom = [
 #     [1, 2, 3],
 #     [4, 5, 6],
@@ -21,15 +23,17 @@
 #     print(sor)
 
 
+# egy masik jo megoldas
+
 # jobbra forgat
-a = [
-    [1, 2, 3, 1],
-    [4, 5, 6, 4],
-    [7, 8, 9, 7],
-    [10, 11, 12, 13]
-]
-for row in a:
-    print(row)
+# a = [
+#     [1, 2, 3, 1],
+#     [4, 5, 6, 4],
+#     [7, 8, 9, 7],
+#     [10, 11, 12, 13]
+# ]
+# for row in a:
+#     print(row)
 
 # b = [
 #     [0, 0, 0, 0],
@@ -38,14 +42,38 @@ for row in a:
 #     [0, 0, 0, 0]
 # ]
 
-b = a.copy()
+# b = a.copy()
 
-print(">>>>>>")
+# print(">>>>>>")
 
-for i in range(len(a)):
-    for j in range(len(a[0])):
-        b[j][(len(a))-1-i] = a[i][j]
+# for i in range(len(a)):
+#     for j in range(len(a[0])):
+#         b[j][(len(a))-1-i] = a[i][j]
 
 
-for row in b:
-    print(row)
+# for row in b:
+#     print(row)
+
+# harmadik jo megoldas
+
+matrix = [
+    [1, 2, 3, 1],
+    [4, 5, 6, 4],
+    [7, 8, 9, 7],
+    [10, 11, 12, 13]
+]
+
+# print(matrix)
+
+for elem in list([list(sor) for sor in zip(*matrix)][:: -1]):
+    print(elem)
+
+# list(list(x) for x in zip(*matrix))[:: -1])
+# list comprehension
+
+# [muvelet(valtozo_elnevezes) for valtozo_elnevezes in miben]
+
+# megegyezik azzal, hogy:
+
+# for valtozo_elnevezes in miben:
+#      muvelet(valtozo_elnevezes)
