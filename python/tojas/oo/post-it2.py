5bc6c3ae93c9a663cd8601e2a4b4c903f4cb385e
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2020-04-25 13:30:46 +0200

    elso feladat konstruktorral

---
+++ python/tojas/oo/post-it2.py
@@ -0,0 +1,9 @@
+class PostIt:
+    def __init__(self, hatter_szin, szoveg, szoveg_szin):
+        self.hatter_szin = hatter_szin
+        self.szoveg = szoveg
+        self.szoveg_szin = szoveg_szin
+
+post_it_1 = PostIt('sarga', 'Elso otlet', 'kek')
+post_it_2 = PostIt('rozsaszin', 'Hurra!', 'fekete')
+post_it_3 = PostIt('zold', 'Szuper!', 'barna')
