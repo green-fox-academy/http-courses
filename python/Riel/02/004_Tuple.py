cc6de65d54f5590feb10acce2b27f16e3dbffaa2
Riel <robert.bor@greenfoxacademy.com>
2020-04-25 14:24:53 +0200

    code for tuple-dict added

---
+++ python/Riel/02/004_Tuple.py
@@ -0,0 +1,40 @@
+# Készítsünk egy Tuple objektumot
+my_tuple = ("kutya", "tacsko", "fekete", 6)
+
+# Vegyük ki az első elemét
+print(my_tuple[0])
+
+# Vegyük ki az első két elemét egy új tuple-ba
+print(my_tuple[:2])
+
+# Hozzunk létre egy új tuple-t, úgy, hogy a régihez hozzáadunk egyszere több elemet
+my_tuple += (True, "játékos")
+print(my_tuple)
+
+# Számoljuk meg, hányszor van a "kutya" elem a tuple-ban
+print(my_tuple.count("kutya"))
+
+# Hányadik indexen található a "tacskó"?
+print(my_tuple.index("tacsko"))
+
+# Módosítsuk a "tacsko" elemet "cica"-ra
+# my_tuple[1] = "vizsla"
+
+
+
+
+
+
+
+
+
+
+
+
+# Vegyük ki az első elemét
+
+# Vegyük ki az első két elemét egy új tuple-ba
+
+# Hozzunk létre egy új tuple-t, úgy, hogy a régihez hozzáadunk egyszere több elemet
+
+# Számoljuk meg, hányszor van a "kutya" elem a tuple-ban
