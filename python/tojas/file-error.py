467b0e4b39701c8e9495e4e876a8b193c57545f8
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2020-04-25 10:51:43 +0200

    error handling

--- python/tojas/file-error.py
+++ python/tojas/file-error.py
@@ -1,3 +1,20 @@
-file = open('nincs.txt', 'r')
-file.read()
-file.close()
+try:
+    print('elotte')
+    #print(1 / 0)
+    file = open('text.txt', 'r')
+    print('utana')
+    #[1,2][4]
+    raise Exception('na')
+    file.read()
+    file.close()
+except FileNotFoundError:
+    print('ajjaj nem sikerult a filet megynyitni...')
+except ZeroDivisionError:
+    print('ne ossza\' nullaval nem vagy csaknorisz')
+except Exception as e:
+    print(e)
+    print('ma nem is tudom mi tortent')
+else:
+    print('else')
+finally:
+    print('vege')
