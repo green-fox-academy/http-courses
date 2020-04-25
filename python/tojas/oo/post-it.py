ecbed95fba124a8a2bfffba90a343647531845f3
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2020-04-25 13:27:25 +0200

    elso feladat

---
+++ python/tojas/oo/post-it.py
@@ -0,0 +1,22 @@
+class PostIt:
+    hatter_szin = ''
+    szoveg = ''
+    szoveg_szin = ''
+
+post_it_1 = PostIt()
+
+post_it_1.hatter_szin = 'sarga'
+post_it_1.szoveg = 'Elso otlet!'
+post_it_1.szoveg_szin = 'kek'
+
+post_it_2 = PostIt()
+
+post_it_2.hatter_szin = 'rozsaszin'
+post_it_2.szoveg = 'Hurra!'
+post_it_2.szoveg_szin = 'fekete'
+
+post_it_3 = PostIt()
+
+post_it_3.hatter_szin = 'zold'
+post_it_3.szoveg = 'Szuper!'
+post_it_3.szoveg_szin = 'barna'
