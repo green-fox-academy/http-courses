2340339763e446457ea33401ed0ee2e774316040
Riel <robert.bor@greenfoxacademy.com>
2020-10-17 08:41:26 +0200

    Add summary for previous day

---
+++ python/Riel/03_ObjectOrientation/00_sum.py
@@ -0,0 +1,75 @@
+#Python jellemzők:
+# Interpretált
+# - nincs előzetes fordítás
+# - egy interpreter direktben futtatja a kódot
+# Objektum orientált
+# - objektumok interakciójára épül
+# Dinamikus típus kezelés
+# - Nem kell meghatároznunk a változó típusát amikor létrehozzuk azt
+# Általános felhasználás
+# - A programozói világ megannyi területén használható
+
+
+
+
+
+
+# Verziók
+# 1-2-3
+# 3-as nem kompatibilis a 2-vel
+
+
+
+
+
+
+
+
+
+# Futtatás
+# python sum.py
+# Interpreter
+# python (verzió)
+# Kilépés:
+# exit()
+
+print("sum-py fut")
+
+
+
+
+
+
+
+
+# Gyűjtemények:
+# Lista []:
+my_list = [1,2,3,4,5,6]
+# `Módosítható`, `Sorrend tartó`, `indexelt`
+
+
+
+
+
+# Tuple ():
+my_tuple = (1, 2, 3)
+# `Immutable`, `Sorrend tartó`, `indexelt`
+
+
+
+
+
+
+# Dictionary {}: 
+my_dict = {1:"cat", 2:"dog"}
+# `Módosítható`, `Nincs sorrend`, `nem indexelt`
+# Kulcs-érték párokból áll
+# A kulcs egyedi kell, hogy legyen
+
+
+
+
+
+
+# String
+my_string = "Kutyuli"
